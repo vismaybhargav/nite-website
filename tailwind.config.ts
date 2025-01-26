bdb2db95f6f9de7709a6b1d95602859aa4fef025
Vismay Bhargav <122339161+vismaybhargav@users.noreply.github.com>
2025-01-26 11:45:41 -0800

    (feat) basis + unpushed code

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,6 +12,10 @@ const config: Config = {
         background: "var(--background)",
         foreground: "var(--foreground)",
       },
+      fontSize: {
+        sm: "0.8rem",
+        base: "1rem",
+      }
     },
   },
   plugins: [],
