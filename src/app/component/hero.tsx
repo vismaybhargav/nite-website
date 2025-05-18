835ebf6340314272f900f0020a8f7701ae2d0d22
Vismay Bhargav <122339161+vismaybhargav@users.noreply.github.com>
2025-05-17 20:22:49 -0700

    fix the motion package

--- src/app/component/hero.tsx
+++ src/app/component/hero.tsx
@@ -1,7 +1,7 @@
 "use client"
 
 import Link from "next/link"
-import { motion, AnimatePresence } from "motion"
+import { motion, AnimatePresence } from "motion/react"
 import { useState, useEffect, useRef } from "react"
 
 export default function Hero() {
